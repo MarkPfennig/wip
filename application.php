e1be7be9cc47bbfaac9ec173536c3480cf7cbe47
MarkPfennig <coinsolidation@gmail.com>
2014-12-06 20:25:09 +0000

    app base

---
+++ application.php
@@ -0,0 +1,10 @@
+<?php
+
+define('MT_DIR_BASE',   __DIR__ . DIRECTORY_SEPARATOR);
+define('MT_DIR_CONFIG', MT_DIR_BASE . 'config' . DIRECTORY_SEPARATOR);
+define('MT_DIR_DATA',   MT_DIR_BASE . 'data' . DIRECTORY_SEPARATOR);
+define('MT_DIR_LIB',    MT_DIR_BASE . 'lib' . DIRECTORY_SEPARATOR);
+define('MT_DIR_VENDOR', MT_DIR_BASE . 'vendor' . DIRECTORY_SEPARATOR);
+define('MT_DIR_WEB',    MT_DIR_BASE . 'web' . DIRECTORY_SEPARATOR);
+
+require_once MT_DIR_LIB . 'core.php';
